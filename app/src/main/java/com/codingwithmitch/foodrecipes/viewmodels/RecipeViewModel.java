55a4ec5a6488468e9964c00ad511c2d84a95f8b0
Shawn <7067shawnnichol@gmail.com>
2020-03-22 17:59:01 -0600

    Initial commit

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeViewModel.java
@@ -0,0 +1,40 @@
+package com.codingwithmitch.foodrecipes.viewmodels;
+
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.annotation.NonNull;
+
+
+public class RecipeViewModel extends AndroidViewModel {
+
+
+    public RecipeViewModel(@NonNull Application application) {
+        super(application);
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
