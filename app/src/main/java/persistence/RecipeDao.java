96688af7a4fdbb4eaf6b0b375e466090a19413e8
Shawn <7067shawnnichol@gmail.com>
2020-03-25 23:36:28 -0600

    06)RecipeDao

--- app/src/main/java/persistence/RecipeDao.java
+++ app/src/main/java/persistence/RecipeDao.java
@@ -21,11 +21,11 @@ public interface RecipeDao  {
     @Insert(onConflict = REPLACE)
     void insertRecipe(Recipe recipe);
 
-    @Query("UPDATE recipes SET title = :title, publisher = :publisher, image_url, social_rank = :social_rank " +
+    @Query("UPDATE recipes SET title = :title, publisher = :publisher, image_url = :image_url, social_rank = :social_rank " +
     "WHERE recipe_id = :recipe_id")
     void updateRecipe(String recipe_id, String title, String publisher, String image_url, float social_rank);
 
-    @Query("SELECT * FROM recipes WHERE title LIKE '%' || :query || '%' OR ingredients LIKE '%' || : query || '%' +" +
+    @Query("SELECT * FROM recipes WHERE title LIKE '%' || :query || '%' OR ingredients LIKE '%' || :query || '%' " +
             "ORDER BY social_rank DESC LIMIT (:pageNumber * 30)")
     LiveData<List<Recipe>> searchRecipes(String query, int pageNumber);
 
