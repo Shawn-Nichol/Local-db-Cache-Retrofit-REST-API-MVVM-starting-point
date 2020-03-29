29d1e498e48b4d674d828e83f9a8fbc968749c6b
Shawn <7067shawnnichol@gmail.com>
2020-03-29 16:08:26 -0600

    19)Refreshing Cached data

--- app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeViewModel.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/viewmodels/RecipeViewModel.java
@@ -2,18 +2,34 @@ package com.codingwithmitch.foodrecipes.viewmodels;
 
 
 import android.app.Application;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.repositories.RecipeRepository;
+import com.codingwithmitch.foodrecipes.util.Resource;
 
 
 public class RecipeViewModel extends AndroidViewModel {
 
+    private static final String TAG = "RecipeViewModel";
+
+    private RecipeRepository mRecipeRepository;
 
     public RecipeViewModel(@NonNull Application application) {
         super(application);
-
+        mRecipeRepository = RecipeRepository.getInstance(application);
     }
 
+    public LiveData<Resource<Recipe>> searchRecipeApi(String recipeId) {
+        return mRecipeRepository.searchRecipesApi(recipeId);
+    }
 
 
 }
